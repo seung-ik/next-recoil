c2f173bf675a7469be7ac1ff1514e219f4795355
seung-ik <5seungik@traum.biz>
2022-03-28 17:58:14 +0900

    complete setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { RecoilRoot } from 'recoil';
 import { GlobalStyle } from "@styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,8 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>boilerplate</title>
       </Head>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   );
 }
